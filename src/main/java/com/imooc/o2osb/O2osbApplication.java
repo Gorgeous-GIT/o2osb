f7d5fddcb673191d5a7a26ec175e34790d1e562a
Gorgeous-IMYun <392555078@qq.com>
2019-08-15 00:21:57 +0800

    2019/8/15 12:22 13.2-13.5 00:27:48

---
+++ src/main/java/com/imooc/o2osb/O2osbApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.o2osb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class O2osbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(O2osbApplication.class, args);
+	}
+
+}
